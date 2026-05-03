b6bb1f7bd468f88246acb3f8995987435ac1faec
Fernando A. <feraldev98@hotmail.com>
2026-05-03 18:58:10 -0500

    ADD: static files

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,75 @@
-import Image from "next/image";
+import { Navbar } from "@/components/Navbar";
+import { profileNameAscii } from "@/static/profileNameAscii";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="w-screen flex flex-col items-center justify-center">
+
+      <div className="border border-primary my-2 rounded-xs w-[calc(100%-1rem)] flex flex-col justify-center items-center py-2">  
+        {/* HEADER */}
+        <div className="border-b border-primary text-sm w-full grid grid-cols-3 gap-2 px-4 py-1">
+          <div className="grid grid-cols-4 gap-2 text-sm w-full col-span-2">
+            <span>FERAL BIOS (TM)</span>
+            <span>© 2026 FERAL</span>
+            <span>VER 1.0.0</span>
+            <span>SYS: ONLINE</span>
+          </div>
+          <div className="grid grid-cols-2 text-sm ml-auto text-right gap-6">
+            <span>PORT: 8080</span>
+            <span>← LANG: EN →</span>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        {/* NAV */}
+        <div className="grid grid-cols-6 gap-2 w-[calc(100%-1rem)] border text-(--primary) border-(--primary) mt-4 mb-2 mx-4 py-1 text-xl">
+          <span className="text-center place-content-center">SUMMARY</span>
+          <span className="text-center place-content-center">PROJECTS</span>
+          <span className="text-center place-content-center">SKILLS</span>
+          <span className="text-center place-content-center">EXPERIENCE</span>
+          <span className="text-center place-content-center">CONTACT</span>
+          <span className="text-right text-xl pr-2 place-content-center text-shadow-2xs text-shadow-sky-300">{'>_TERMINAL'}</span>
         </div>
-      </main>
+
+        {/* TERMINAL & ASCII IMG */}
+        <div className="grid grid-cols-5 gap-1 w-[calc(100%-1rem)] text-(--primary) mx-4 pt-1">
+          <div className="col-span-2 border border-(--primary) flex flex-col gap-1 px-2 py-1">
+            <span className="text-base">{'> SYSTEM.frl'}</span>
+            <span className="text-sm">Initializing developer profile...</span>
+            <div className="flex flex-col">
+              <pre className="text-xs w-full text-center">
+                {profileNameAscii}
+              </pre>
+              <span className="text-center uppercase"> engineer / programmer / teacher </span>
+            </div>
+            <div className="flex flex-col mt-4">
+              <span className="text-base">{'> INFO.TXT'}</span>
+              <span className="pl-4 pt-1">{'> Name: John Doe'}</span>
+              <span className="pl-4">{'> Location: Cajamarca, Perú'}</span>
+              <span className="pl-4">{'> Availability: Available for work'}</span>
+              <span className="pl-4">{'> Focus: Full-Stack Development'}</span>
+              <span className="pl-4">{'> Status: Building cool things'}</span>
+            </div>
+            <div className="flex flex-col mt-4">
+              <span>{'> ABOUT_ME.TXT'}</span>
+              <span className="pl-4 py-1">{'I am a passionate developer with experience in full-stack development. I love creating innovative solutions and teaching others.'}</span>
+              <span className="pl-4 py-1">{'Tech enthusiast and lifelong learner.'}</span>
+            </div>
+            <div className="flex flex-col mt-4">
+              <span className="text-base">{'> CURRENT_STACK.TXT'}</span>
+              <div className="flex flex-wrap gap-2 items-center justify-center px-2 py-2 text-sm">
+                <span className="px-1">{'> NextJS'}</span>
+                <span className="px-1">{'> TypeScript'}</span>
+                <span className="px-1">{'> TailwindCSS'}</span>
+                <span className="px-1">{'> ReactJS'}</span>
+                <span className="px-1">{'> FramerMotion'}</span>
+                <span className="px-1">{'> Git & Github'}</span>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-3 border border-(--primary)">
+            
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
